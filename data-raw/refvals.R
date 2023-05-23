4bd1ba9359352529b77c7c3f3ebf69f782636517
mncube <macksonncube@gmail.com>
2023-05-23 23:56:17 +0000

    start importing data

---
+++ data-raw/refvals.R
@@ -0,0 +1,12 @@
+## code to prepare `refvals` dataset goes here
+require(dplyr)
+refvals <- readxl::read_excel("./rawdatafiles/Data for MixSIAR.xlsx",
+                              sheet = "Reference Sources") %>%
+  dplyr::rename(d15N = δ15N, d13C = δ13C) %>%
+  dplyr::mutate(Group = ifelse(is.na(d15N) & is.na(d13C), Organism, NA)) %>%
+  dplyr::filter(!is.na(Organism)) %>%
+  dplyr::filter(Organism != "Organism") %>%
+  tidyr::fill(Group, .direction = "down") %>%
+  dplyr::filter(!(is.na(d15N) & is.na(d13C)))
+
+usethis::use_data(refvals, overwrite = TRUE)
