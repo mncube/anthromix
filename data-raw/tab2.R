05a00145130bd2a523b58197fb5a9e616c43c536
mncube <macksonncube@gmail.com>
2023-05-28 17:45:37 +0000

    update tab2

--- data-raw/tab2.R
+++ data-raw/tab2.R
@@ -3,6 +3,8 @@ require(dplyr)
 tab2 <- readxl::read_excel("./rawdatafiles/Data for MixSIAR.xlsx",
                               sheet = "OM Full Comparison") %>%
   dplyr::select(Cemetary, UniqueID = `Unique ID`, d15N = δ15N, d13C = δ13C,
-                ArmPosPeriod = `Arm Pos/Period`, Stat)
+                ArmPosPeriod = `Arm Pos/Period`, Stat) %>%
+  dplyr::mutate(d15N = as.numeric(d15N),
+                d13C = as.numeric(d13C))
 
 usethis::use_data(tab2, overwrite = TRUE)
