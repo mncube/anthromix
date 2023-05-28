8e1e022ff841e6cf97c00c52c224d34c88a75c7a
mncube <macksonncube@gmail.com>
2023-05-28 22:10:53 +0000

    tab data

--- data-raw/tab2.R
+++ data-raw/tab2.R
@@ -11,6 +11,19 @@ tab2 <- readxl::read_excel("./rawdatafiles/Data for MixSIAR.xlsx",
                               ~stringr::str_replace(.,"−", "-"))) %>%
   dplyr::mutate(d15N = readr::parse_number(d15N),
                 d13C = readr::parse_number(d13C)) %>%
-  tidyr::drop_na(d15N, d13C)
+  tidyr::drop_na(d15N, d13C) %>%
+  dplyr::mutate(Period = dplyr::case_when(Period == "a" ~ "A",
+                                   Period == "b" ~ "B",
+                                   Period == "c" ~ "C",
+                                   Period == "d" ~ "D",
+                                   TRUE ~ NA)) %>%
+  dplyr::mutate(Period = factor(Period, levels = c("A", "B", "C", "D"),
+                                   labels = c("A", "B", "C", "D"))) %>%
+  dplyr::mutate(Stat = dplyr::case_when(Stat == 0 ~ "Peasant",
+                                        Stat == 1 ~ "Elite",
+                                        Stat == 2 ~ "Monk",
+                                        TRUE ~ NA)) %>%
+  dplyr::mutate(Stat = factor(Stat, levels = c("Peasant", "Elite", "Monk"),
+                                 labels = c("Peasant", "Elite", "Monk")))
 
 usethis::use_data(tab2, overwrite = TRUE)
