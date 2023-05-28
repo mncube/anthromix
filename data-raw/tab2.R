71f8f9d6e0a11bd452f0f3de0bae44ddb58c0865
mncube <macksonncube@gmail.com>
2023-05-28 17:27:10 +0000

    tab2 data

---
+++ data-raw/tab2.R
@@ -0,0 +1,8 @@
+## code to prepare `tab2` dataset goes here
+require(dplyr)
+tab2 <- readxl::read_excel("./rawdatafiles/Data for MixSIAR.xlsx",
+                              sheet = "OM Full Comparison") %>%
+  dplyr::select(Cemetary, UniqueID = `Unique ID`, d15N = δ15N, d13C = δ13C,
+                ArmPosPeriod = `Arm Pos/Period`, Stat)
+
+usethis::use_data(tab2, overwrite = TRUE)
